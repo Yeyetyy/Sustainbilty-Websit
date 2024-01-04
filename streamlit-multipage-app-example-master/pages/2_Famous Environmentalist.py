81970e5909751349342373a9f76aee8ec033052a
Yeyetyy <147157244+Yeyetyy@users.noreply.github.com>
2024-01-04 18:56:26 +0100

    Update 2_Famous Environmentalist.py

--- streamlit-multipage-app-example-master/pages/2_Famous Environmentalist.py
+++ streamlit-multipage-app-example-master/pages/2_Famous Environmentalist.py
@@ -12,7 +12,7 @@ st.image('streamlit-multipage-app-example-master/Greta_Thunberg_02_cropped.jpg',
 #text about the person
 Click = st.button("Reveal Info About Greta Thunberg")
 
-if Click:
+if Click is True:
 
     st.title("About")
     st.text("Born: January 3, 2003 (age 20 years), Stockholm, Sweden")
@@ -21,3 +21,6 @@ if Click:
     st.text("Movement: School Strike for Climate")
     st.write("Awards List [link](https://en.wikipedia.org/wiki/Greta_Thunberg#Honours_and_awards:~:text=COVID%2D19%20pandemic.-,Honours%20and%20awards,doctorate%20was%20scheduled%20to%20be%20granted%20in%20June%202023.%5B301%5D,-Species%20named%20in)")
     st.sidebar.success("Choose a page.")
+
+    else 
+    st.write("click the button")
