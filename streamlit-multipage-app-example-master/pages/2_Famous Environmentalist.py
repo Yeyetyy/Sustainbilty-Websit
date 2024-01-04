2ce50cb3996332788284c0b125ac382a3044f699
Yeyetyy <147157244+Yeyetyy@users.noreply.github.com>
2024-01-04 19:01:49 +0100

    Update 2_Famous Environmentalist.py

--- streamlit-multipage-app-example-master/pages/2_Famous Environmentalist.py
+++ streamlit-multipage-app-example-master/pages/2_Famous Environmentalist.py
@@ -1,19 +1,16 @@
-
 import streamlit as st
-
 
 st.title("Famous Environmentalist ")
 
+# The image that pops up onto the screen
+st.image('streamlit-multipage-app-example-master/Greta_Thunberg_02_cropped.jpg', caption="Greta Thunberg")
 
+# Text about the person
+click_button = st.button("Reveal Info About Greta Thunberg")
 
-#the image that pops up onto the screen
-st.image('streamlit-multipage-app-example-master/Greta_Thunberg_02_cropped.jpg', caption= "Greta Thunberg")
-
-#text about the person
-Click = st.button("Reveal Info About Greta Thunberg")
-
-if Click:
-
+# Check if the button is clicked
+if click_button:
+    # Display information when the button is clicked
     st.title("About")
     st.text("Born: January 3, 2003 (age 20 years), Stockholm, Sweden")
     st.text("Parents: Svante Thunberg, Malena Ernman")
@@ -21,5 +18,6 @@ if Click:
     st.text("Movement: School Strike for Climate")
     st.write("Awards List [link](https://en.wikipedia.org/wiki/Greta_Thunberg#Honours_and_awards:~:text=COVID%2D19%20pandemic.-,Honours%20and%20awards,doctorate%20was%20scheduled%20to%20be%20granted%20in%20June%202023.%5B301%5D,-Species%20named%20in)")
     st.sidebar.success("Choose a page.")
-
-
+else:
+    # Optionally display some initial information
+    st.info("Click the button to reveal information about Greta Thunberg.")
