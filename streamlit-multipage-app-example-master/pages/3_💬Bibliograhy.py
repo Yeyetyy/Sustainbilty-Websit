946273ed43e0a024f7c0bea224aca123e8284afb
Yeyetyy <147157244+Yeyetyy@users.noreply.github.com>
2024-01-06 01:15:15 +0100

    Delete streamlit-multipage-app-example-master directory

--- streamlit-multipage-app-example-master/pages/3_💬Bibliograhy.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("Bibliograhy")
-st.text("“What Is Sustainability? | UCLA Sustainability.” UCLA Sustainability, 2023, www.sustain.ucla.edu/what-is-sustainability/. Accessed 13 Dec. 2023.")
-st.text("“Why Is Sustainability Important? | BluGlacier - Top-Quality Salmon Producer.” BluGlacier, 15 July 2020, bluglacier.com/why-is-sustainability-important/#:~:text=Sustainability%20improves%20the%20quality%20of,to%20logistics%20to%20customer%20service. Accessed 14 Dec. 2023.")
-st.sidebar.success("Choose a page.")
-
-#(“Why Is Sustainability Important? | BluGlacier - Top-Quality Salmon Producer”)'
