02cdb14e4541a1eee219819389460b1e0a24d4f0
Yeyetyy <147157244+Yeyetyy@users.noreply.github.com>
2024-01-13 00:51:07 +0100

    Create 5_Review.py

---
+++ streamlit-multipage-app-example-master/pages/5_Review.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Review On Website")
+st.write('https://docs.google.com/forms/d/e/1FAIpQLSfT-IOvVQgubi0V1iqgbC8tapRCDUcorJil-I17Z-BNgrOvuA/viewform?usp=sf_link[https://docs.google.com/forms/d/e/1FAIpQLSfT-IOvVQgubi0V1iqgbC8tapRCDUcorJil-I17Z-BNgrOvuA/viewform?usp=sf_link]')
